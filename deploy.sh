80e3070fb1e7af1326f10acba6e02f062ebadc5b
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2023-10-13 12:56:18 +0200

    feat: add the 8th track

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-scp -q -i ~/.ssh/id_rsa *.pd *.py patch@192.168.1.29:/usr/local/puredata-patches/HeptaRecorder/
+scp -q -i ~/.ssh/id_rsa *.pd *.py patch@192.168.1.29:/usr/local/puredata-patches/OctoRecorder/
 read -p "Done uploading. Run program? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	ssh -i ~/.ssh/id_rsa patch@192.168.1.29 "sh /usr/local/patchbox-modules/puredata/launch.sh /usr/local/puredata-patches/HeptaRecorder/main.pd"
+	ssh -i ~/.ssh/id_rsa patch@192.168.1.29 "sh /usr/local/patchbox-modules/puredata/launch.sh /usr/local/puredata-patches/OctoRecorder/main.pd"
 fi
